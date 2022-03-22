b9b354a96622e2c561467460bb02f46316345e67
wangqi <1942460489@qq.com>
2022-03-22 08:28:04 +0800

    rest endpoint.

--- flink-gateway-engine/src/main/java/cn/sliew/flink/gateway/engine/RestEndpoint.java
+++ flink-gateway-engine/src/main/java/cn/sliew/flink/gateway/engine/RestEndpoint.java
@@ -1,9 +1,20 @@
 package cn.sliew.flink.gateway.engine;
 
+import org.apache.flink.runtime.messages.webmonitor.JobIdsWithStatusOverview;
+import org.apache.flink.runtime.messages.webmonitor.MultipleJobsDetails;
+import org.apache.flink.runtime.rest.handler.async.AsynchronousOperationResult;
+import org.apache.flink.runtime.rest.handler.async.TriggerResponse;
+import org.apache.flink.runtime.rest.messages.ClusterConfigurationInfoEntry;
 import org.apache.flink.runtime.rest.messages.DashboardConfiguration;
+import org.apache.flink.runtime.rest.messages.JobPlanInfo;
+import org.apache.flink.runtime.rest.messages.LogListInfo;
 import org.apache.flink.runtime.rest.messages.dataset.ClusterDataSetListResponseBody;
+import org.apache.flink.runtime.rest.messages.job.JobSubmitRequestBody;
+import org.apache.flink.runtime.rest.messages.job.JobSubmitResponseBody;
+import org.apache.flink.runtime.webmonitor.handlers.*;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface RestEndpoint {
 
@@ -21,6 +32,83 @@ public interface RestEndpoint {
      * Returns all cluster data sets.
      */
     ClusterDataSetListResponseBody datasets() throws IOException;
+
+    /**
+     * Triggers the deletion of a cluster data set.
+     * This async operation would return a 'triggerid' for further query identifier.
+     */
+    TriggerResponse deleteDataSet(String datasetId) throws IOException;
+
+    /**
+     * Returns the status for the delete operation of a cluster data set.
+     */
+    AsynchronousOperationResult deleteDataSetStatus(String triggerId) throws IOException;
+
+    /**
+     * Returns a list of all jars previously uploaded via '/jars/upload'.
+     */
+    JarListInfo jars() throws IOException;
+
+    /**
+     * Uploads a jar to the cluster.
+     * The jar must be sent as multi-part data.
+     * Make sure that the "Content-Type" header is set to "application/x-java-archive", as some http libraries do not add the header by default.
+     * Using 'curl' you can upload a jar via 'curl -X POST -H "Expect:" -F "jarfile=@path/to/flink-job.jar" http://hostname:port/jars/upload'.
+     */
+    JarUploadResponseBody uploadJar(String filePath) throws IOException;
+
+    /**
+     * Deletes a jar previously uploaded via '/jars/upload'.
+     */
+    boolean deleteJar(String jarId) throws IOException;
+
+    /**
+     * Returns the dataflow plan of a job contained in a jar previously uploaded via '/jars/upload'.
+     * Program arguments can be passed both via the JSON request (recommended) or query parameters.
+     */
+    JobPlanInfo jarPlan(String jarId, JarPlanRequestBody requestBody) throws IOException;
+
+    /**
+     * Submits a job by running a jar previously uploaded via '/jars/upload'.
+     * Program arguments can be passed both via the JSON request (recommended) or query parameters.
+     */
+    JarRunResponseBody jarRun(String jarId, JarRunRequestBody requestBody) throws IOException;
+
+    /**
+     * Returns the cluster configuration.
+     */
+    List<ClusterConfigurationInfoEntry> jobmanagerConfig() throws IOException;
+
+    /**
+     * Returns the list of log files on the JobManager.
+     */
+    LogListInfo jobmanagerLogs() throws IOException;
+
+    /**
+     * Provides access to job manager metrics.
+     */
+    String jobmanagerMetrics(String metric) throws IOException;
+
+    /**
+     * Returns an overview over all jobs and their current state.
+     */
+    JobIdsWithStatusOverview jobs() throws IOException;
+
+    /**
+     * Submits a job.
+     * This call is primarily intended to be used by the Flink client.
+     * This call expects a multipart/form-data request that consists of file uploads for the serialized JobGraph,
+     * jars and distributed cache artifacts and an attribute named "request" for the JSON payload.
+     */
+    JobSubmitResponseBody jobSubmit(JobSubmitRequestBody requestBody) throws IOException;
+
+    String jobsMetric(String get, String agg, String jobs) throws IOException;
+
+    /**
+     * Returns an overview over all jobs.
+     */
+    MultipleJobsDetails jobsOverview() throws IOException;
+
 
 
 }
--- flink-gateway-engine/src/main/java/cn/sliew/flink/gateway/engine/impl/RestEndpointImpl.java
+++ flink-gateway-engine/src/main/java/cn/sliew/flink/gateway/engine/impl/RestEndpointImpl.java
@@ -3,15 +3,31 @@ package cn.sliew.flink.gateway.engine.impl;
 import cn.sliew.flink.gateway.engine.RestEndpoint;
 import cn.sliew.milky.common.util.JacksonUtil;
 import org.apache.flink.calcite.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.runtime.messages.webmonitor.JobIdsWithStatusOverview;
+import org.apache.flink.runtime.messages.webmonitor.MultipleJobsDetails;
+import org.apache.flink.runtime.rest.handler.async.AsynchronousOperationResult;
+import org.apache.flink.runtime.rest.handler.async.TriggerResponse;
+import org.apache.flink.runtime.rest.messages.ClusterConfigurationInfoEntry;
 import org.apache.flink.runtime.rest.messages.DashboardConfiguration;
+import org.apache.flink.runtime.rest.messages.JobPlanInfo;
+import org.apache.flink.runtime.rest.messages.LogListInfo;
 import org.apache.flink.runtime.rest.messages.dataset.ClusterDataSetListResponseBody;
+import org.apache.flink.runtime.rest.messages.job.JobSubmitRequestBody;
+import org.apache.flink.runtime.rest.messages.job.JobSubmitResponseBody;
+import org.apache.flink.runtime.webmonitor.handlers.*;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.fluent.Content;
 import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.util.Timeout;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class RestEndpointImpl implements RestEndpoint {
 
@@ -39,7 +55,7 @@ public class RestEndpointImpl implements RestEndpoint {
                 .responseTimeout(Timeout.ofSeconds(3L))
                 .execute().returnContent();
         // 不顶用再说
-        return JacksonUtil.parseJsonString(content.asString(), DashboardConfiguration.class);
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), DashboardConfiguration.class);
     }
 
     @Override
@@ -48,6 +64,139 @@ public class RestEndpointImpl implements RestEndpoint {
                 .connectTimeout(Timeout.ofSeconds(3L))
                 .responseTimeout(Timeout.ofSeconds(3L))
                 .execute().returnContent();
-        return JacksonUtil.parseJsonString(content.asString(), ClusterDataSetListResponseBody.class);
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), ClusterDataSetListResponseBody.class);
     }
+
+    @Override
+    public TriggerResponse deleteDataSet(String datasetId) throws IOException {
+        Content content = Request.delete(webInterfaceURL + "/datasets/" + datasetId)
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), TriggerResponse.class);
+    }
+
+    @Override
+    public AsynchronousOperationResult deleteDataSetStatus(String triggerId) throws IOException {
+        Content content = Request.get(webInterfaceURL + "/datasets/delete/" + triggerId)
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), AsynchronousOperationResult.class);
+    }
+
+    @Override
+    public JarListInfo jars() throws IOException {
+        Content content = Request.get(webInterfaceURL + "/jars")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), JarListInfo.class);
+    }
+
+    @Override
+    public JarUploadResponseBody uploadJar(String filePath) throws IOException {
+        File jarFile = new File(filePath);
+        Content content = Request.post(webInterfaceURL + "/jars/upload")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .body(
+                        MultipartEntityBuilder.create()
+                                .addBinaryBody("jarfile", jarFile, ContentType.create("application/java-archive"), jarFile.getName())
+                                .build()
+                ).execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), JarUploadResponseBody.class);
+    }
+
+    @Override
+    public boolean deleteJar(String jarId) throws IOException {
+        HttpResponse response = Request.delete(webInterfaceURL + "/jars/" + jarId)
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnResponse();
+        return response.getCode() == HttpStatus.SC_SUCCESS;
+    }
+
+    @Override
+    public JobPlanInfo jarPlan(String jarId, JarPlanRequestBody requestBody) throws IOException {
+        Content content = Request.post(webInterfaceURL + "/jars/" + jarId + "/plan")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .body(new StringEntity(JacksonUtil.toJsonString(requestBody)))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), JobPlanInfo.class);
+    }
+
+    @Override
+    public JarRunResponseBody jarRun(String jarId, JarRunRequestBody requestBody) throws IOException {
+        Content content = Request.post(webInterfaceURL + "/jars/" + jarId + "/run")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .body(new StringEntity(JacksonUtil.toJsonString(requestBody)))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), JarRunResponseBody.class);
+    }
+
+    @Override
+    public List<ClusterConfigurationInfoEntry> jobmanagerConfig() throws IOException {
+        Content content = Request.get(webInterfaceURL + "/jobmanager/config")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonArray(content.asString(StandardCharsets.UTF_8), ClusterConfigurationInfoEntry.class);
+    }
+
+    @Override
+    public LogListInfo jobmanagerLogs() throws IOException {
+        Content content = Request.get(webInterfaceURL + "/jobmanager/logs")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), LogListInfo.class);
+    }
+
+    @Override
+    public String jobmanagerMetrics(String metric) throws IOException {
+        Content content = Request.get(webInterfaceURL + "/jobmanager/metrics?get=" + metric)
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return content.asString(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public JobIdsWithStatusOverview jobs() throws IOException {
+        Content content = Request.get(webInterfaceURL + "/jobs")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), JobIdsWithStatusOverview.class);
+    }
+
+    @Override
+    public JobSubmitResponseBody jobSubmit(JobSubmitRequestBody requestBody) throws IOException {
+        Content content = Request.post(webInterfaceURL + "/jobs")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .body(new StringEntity(JacksonUtil.toJsonString(requestBody)))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), JobSubmitResponseBody.class);
+    }
+
+    @Override
+    public String jobsMetric(String get, String agg, String jobs) throws IOException {
+        return null;
+    }
+
+    @Override
+    public MultipleJobsDetails jobsOverview() throws IOException {
+        Content content = Request.get(webInterfaceURL + "/jobs/overview")
+                .connectTimeout(Timeout.ofSeconds(3L))
+                .responseTimeout(Timeout.ofSeconds(3L))
+                .execute().returnContent();
+        return JacksonUtil.parseJsonString(content.asString(StandardCharsets.UTF_8), MultipleJobsDetails.class);
+    }
+
+
 }
