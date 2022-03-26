02f883c0eead88cc376c6c8a1b1989adda814ee0
wangqi <1942460489@qq.com>
2022-03-26 21:53:01 +0800

    rest endpoint.

--- flink-gateway-engine/src/main/java/cn/sliew/flink/gateway/engine/ParameterHandler.java
+++ flink-gateway-engine/src/main/java/cn/sliew/flink/gateway/engine/ParameterHandler.java
@@ -6,8 +6,7 @@ import org.apache.flink.configuration.Configuration;
 /**
  * 配置的分组和排序。
  * 声明那些是必需，那些是可选的。
- * 可以组合成装饰器，或者类似 spring security dsl 那种
- * 异或者是组合模式？
+ * 或者类似 spring security dsl 那种，异或者是组合模式？
  * @param <T>
  */
 public interface ParameterHandler<T> {
