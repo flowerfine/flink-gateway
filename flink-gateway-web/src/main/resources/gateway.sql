56fe6f67a5573e0a2aa43129660246e278e4281b
wangqi <1942460489@qq.com>
2022-04-07 08:38:37 +0800

    format sql.

--- flink-gateway-web/src/main/resources/gateway.sql
+++ flink-gateway-web/src/main/resources/gateway.sql
@@ -104,13 +104,28 @@ CREATE TABLE `flink_deploy_log`
     KEY `idx_update_time` (`update_time`)
 ) ENGINE = InnoDB COMMENT ='flink 部署日志';
 
-create table flink_job
+create table flink_artifact
+(
+    `id`          bigint       NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `name`        varchar(255) NOT NULL COMMENT 'artifact 名称',
+    `url`         varchar(255) NOT NULL COMMENT 'artifact 链接。支持 file、hdfs、s3 协议',
+    `creator`     varchar(64)  NOT NULL DEFAULT 'system' COMMENT '创建人 ',
+    `updater`     varchar(64)  NOT NULL DEFAULT 'system' COMMENT '修改者',
+    `deleted`     tinyint      NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
+    `create_time` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    `comments`    varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
+    PRIMARY KEY (`id`),
+    KEY `idx_update_time` (`update_time`)
+) ENGINE = InnoDB COMMENT ='flink artifact';
+
+create table flink_job_config
 (
     `id`                bigint       NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `name`              tinyint(4)   NOT NULL COMMENT '任务名称',
-    `deploy_log_id`     varchar(64) COMMENT '集群地址',
-    `web_interface_url` bigint       NOT NULL COMMENT 'flink web-ui 地址',
-    `status`            tinyint(4)   NOT NULL COMMENT '集群状态。运行或者关闭',
+    `checkpoint`              tinyint(4)   NOT NULL COMMENT '任务名称',
+    `savepoint`     varchar(64) COMMENT '集群地址',
+    `ha` bigint       NOT NULL COMMENT 'flink web-ui 地址',
+    `default`           tinyint(4)   NOT NULL COMMENT '默认标识。0: 默认配置, 1: 自定义',
     `creator`           varchar(64)  NOT NULL DEFAULT 'system' COMMENT '创建人 ',
     `updater`           varchar(64)  NOT NULL DEFAULT 'system' COMMENT '修改者',
     `deleted`           tinyint      NOT NULL DEFAULT '0' COMMENT '删除标识。0: 未删除, 1: 已删除',
@@ -119,8 +134,7 @@ create table flink_job
     `comments`          varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
     PRIMARY KEY (`id`),
     KEY `idx_update_time` (`update_time`)
-) ENGINE = InnoDB COMMENT ='flink 任务';
-
+) ENGINE = InnoDB COMMENT ='flink 任务配置';
 
 -- 任务本身的信息: jar，entryClass，classpath，并行度
 -- savepoint:
