3ba8117b5747abb2e16e79e878fd42b707aa8299
wangqi <1942460489@qq.com>
2022-04-10 20:32:15 +0800

    event system.

--- flink-gateway-event/flink-gateway-event-scheduler/src/main/java/cn/sliew/flink/gateway/event/scheduler/RoundRobinEventSource.java
+++ flink-gateway-event/flink-gateway-event-scheduler/src/main/java/cn/sliew/flink/gateway/event/scheduler/RoundRobinEventSource.java
@@ -1,7 +1,9 @@
 package cn.sliew.flink.gateway.event.scheduler;
 
+/**
+ * 调度器不断执行轮询任务，产生事件，输出事件到固定的地方。
+ */
 public interface RoundRobinEventSource extends EventSource {
 
     Scheduler getScheduler();
-
 }
--- flink-gateway-event/flink-gateway-event-scheduler/src/main/java/cn/sliew/flink/gateway/event/scheduler/Scheduler.java
+++ flink-gateway-event/flink-gateway-event-scheduler/src/main/java/cn/sliew/flink/gateway/event/scheduler/Scheduler.java
@@ -7,13 +7,7 @@ import java.util.concurrent.ScheduledExecutorService;
  * netty 的 eventloop。eventloop 是 jdk 线程池的实现。
  * 时间轮
  * 或者是 quartz
- *
- * 调度器不断执行轮询任务，产生事件，输出事件到固定的地方。
- *
- *
  */
 public interface Scheduler extends ScheduledExecutorService {
-
-
 
 }
